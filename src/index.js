09be8574566943b1281d9ba07219d2004605ecdf
jys <runnway@naver.com>
2021-01-25 02:00:54 +0900

    redux dispatch/subscribe

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (count = 0) => {
-  return count;
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "Add"){
+    return count + 1;
+  } else if (action.type === "Minus") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
 const countStore = createStore(countModifier);
-console.log(countStore.getState());
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+
+countStore.subscribe(onChange);
+
+const hadleAdd = () => {
+  countStore.dispatch({type:"Add"});
+}
+
+add.addEventListener("click", hadleAdd);
+minus.addEventListener("click", () => countStore.dispatch({type:"Minus"}));
