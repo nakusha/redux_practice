e651809ede7e3eaf674eaa658c86825f15e707da
jys <runnway@naver.com>
2021-01-25 02:20:15 +0900

    reCap actions,dispatch

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 number.innerText = 0;
+const CounterType = { 
+  ADD:"Add",
+  MINUS:"Minus"
+}
+
 
 const countModifier = (count = 0, action) => {
-  if (action.type === "Add"){
-    return count + 1;
-  } else if (action.type === "Minus") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case CounterType.ADD:
+      return count + 1;
+    case CounterType.MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -25,8 +31,8 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const hadleAdd = () => {
-  countStore.dispatch({type:"Add"});
+  countStore.dispatch({type:CounterType.ADD});
 }
 
 add.addEventListener("click", hadleAdd);
-minus.addEventListener("click", () => countStore.dispatch({type:"Minus"}));
+minus.addEventListener("click", () => countStore.dispatch({type:CounterType.MINUS}));
