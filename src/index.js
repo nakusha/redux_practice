8082685cfdd7366204ab7ea6ddec642afe34a0d0
jys <runnway@naver.com>
2021-01-25 01:45:56 +0900

    create redux store, reducer

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
-// import { createStore } from "redux";
+import { createStore } from "redux";
 
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// const reducer = () => {};
+const countModifier = (count = 0) => {
+  return count;
+};
 
-// const store = createStore(reducer);
-
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
-}
-
-const handleAdd = () => {
-  count = count + 1;
-  updateText();
-}
-
-const handleMinus = () => {
-  count = count - 1;
-  updateText();
-}
-
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
+const countStore = createStore(countModifier);
+console.log(countStore.getState());
